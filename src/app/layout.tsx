0b6ac42df2dc071a618a219a0bb8f83422cf3400
Alirezaw <Alirezawmoradi@gmail.com>
2024-07-14 20:38:04 +0330

    application wrapped in provider component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,19 @@
-import { Inter } from "next/font/google";
+import {Inter} from "next/font/google";
 import "./globals.css";
+import {Provider} from "@/components/Provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <Provider>
+            <html lang="en">
+            <body className={inter.className}>{children}</body>
+            </html>
+        </Provider>
+    );
 }
