bce30f7b8aba52e70fbd1ace4d13b05ccd8ea75c
Alirezaw <Alirezawmoradi@gmail.com>
2024-07-14 20:37:40 +0330

    auth button created for testing github auth flow

---
+++ src/components/auth-botton.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import {signIn, signOut, useSession} from "next-auth/react";
+
+export const AuthButton = () => {
+    const {data: session} = useSession();
+    return (
+        <>
+            <div className='flex items-center justify-center gap-5'>
+                {
+                    session?.user?.email ?
+                        (<button onClick={() => signOut()}>SignOut</button>)
+                        :
+                        (<button onClick={() => signIn()}>SignIn</button>)
+                }
+            </div>
+        </>
+    )
+}
