4f15b326fbececc4cc26fe4dd0bc568f47e5f9e6
Alirezaw <Alirezawmoradi@gmail.com>
2024-07-14 20:38:13 +0330

    refactored

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+'use client';
+import {AuthButton} from "@/components/auth-botton";
+import {useSession} from "next-auth/react";
 export default function Home() {
-  return (
-    <div className='flex w-full justify-center items-center'>This is Main Page</div>
-  );
+    const {data: session} = useSession();
+    return (
+        <>
+            <div className='flex w-full justify-center items-center'>This is Main Page</div>
+            <AuthButton/>
+            {session?.user?.name}
+            {session?.user?.email}
+            <img src={`${session?.user?.image}`} alt='image' width={256} height={256} />
+        </>
+    );
 }
