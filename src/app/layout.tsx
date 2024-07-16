6a3ecba9ac59e818318ac4308fbeafc261f06e2d
Alirezaw <Alirezawmoradi@gmail.com>
2024-07-16 15:48:59 +0330

    refactored

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import {Inter} from "next/font/google";
 import "./globals.css";
-import {Provider} from "@/components/Provider";
+import {Provider} from "@/components/provider/Provider";
 
 const inter = Inter({subsets: ["latin"]});
 
