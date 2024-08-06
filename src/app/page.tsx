c4d305f7ec62b99a1d8ee1ab976eee1a61d2752c
Alirezaw <Alirezawmoradi@gmail.com>
2024-08-06 23:19:08 +0330

    refactored

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,10 @@
-'use client';
 import {AuthButton} from "@/components/auth-button/auth-button";
-import {useSession} from "next-auth/react";
-import axios from "axios";
 
-export default function Home() {
-    const {data: session} = useSession();
-    const handleClick = async () => {
-        try {
-            const result = await axios(`https://api.github.com/users/username`)
-            console.log(result);
-        }catch (error){
-            console.log(error);
-        }
-    }
+export default async function Home() {
     return (
         <>
             <div className='flex w-full justify-center items-center'>This is Main Page</div>
-            <AuthButton/>
-            {session?.user?.name}
-            {session?.user?.email}
-            <img src={`${session?.user?.image}`} alt='image' width={256} height={256}/>
-            <button onClick={handleClick}>Log data</button>
         </>
+
     );
 }
