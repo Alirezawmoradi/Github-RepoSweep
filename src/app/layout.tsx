ffd85c9f98b183051bf738fce065f22f87cfcb99
Alirezaw <Alirezawmoradi@gmail.com>
2024-08-11 18:49:42 +0330

    refactored

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,10 @@ import {SessionProvider} from "next-auth/react";
 import {Stars} from "@/components/stars";
 import {Poppins} from "next/font/google";
 
-const poppins=Poppins({
+const poppins = Poppins({
     display: 'swap',
     subsets: ['latin'],
-    weight: ['100','200','300', '400', '500', '600', '700', '800', '900'],
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
     variable: '--font-poppins'
 })
 
@@ -18,12 +18,8 @@ export default async function RootLayout({
 }>) {
     const session = await auth()
     return (
-        <html lang="en"  className={`${poppins.variable}`}>
+        <html lang="en" className={`${poppins.variable}`}>
         <body className='min-h-screen flex flex-row'>
-        <img className="fixed bottom-0 left-0 w-full h-full object-cover d-block pointer-events-none" width="801"
-             height="807" loading="lazy" decoding="async" alt="" aria-hidden="true"
-             src="https://github.githubassets.com/images/modules/site/home-campaign/footer-galaxy.jpg"/>
-        <Stars/>
         <SessionProvider session={session}>
             {children}
         </SessionProvider>
