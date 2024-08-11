00156e79299ead71b00ee1d6c7a0fc244be865e3
Alirezaw <Alirezawmoradi@gmail.com>
2024-08-11 17:50:07 +0330

    Poppins font added to project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
-import {Inter} from "next/font/google";
 import "./globals.css";
 import {auth} from "@/auth";
 import {SessionProvider} from "next-auth/react";
 import {Stars} from "@/components/stars";
+import {Poppins} from "next/font/google";
 
-const inter = Inter({subsets: ["latin"]});
+const poppins=Poppins({
+    display: 'swap',
+    subsets: ['latin'],
+    weight: ['100','200','300', '400', '500', '600', '700', '800', '900'],
+    variable: '--font-poppins'
+})
 
 export default async function RootLayout({
                                              children,
@@ -13,7 +18,7 @@ export default async function RootLayout({
 }>) {
     const session = await auth()
     return (
-        <html lang="en">
+        <html lang="en"  className={`${poppins.variable}`}>
         <body className='min-h-screen flex flex-row'>
         <img className="fixed bottom-0 left-0 w-full h-full object-cover d-block pointer-events-none" width="801"
              height="807" loading="lazy" decoding="async" alt="" aria-hidden="true"
