73ddcdb25c59cdaf4dd801d68b41ea51202f2bb2
Alirezaw <Alirezawmoradi@gmail.com>
2024-08-16 03:40:49 +0330

    cards type created

---
+++ src/components/card/card.types.ts
@@ -0,0 +1,10 @@
+interface CardProps {
+    name: string;
+    description: string | null;
+    isPrivate: boolean;
+    url: string;
+    onSelect: () => void;
+    isSelected: boolean;
+    language: string;
+    stargazers_count: number
+}
