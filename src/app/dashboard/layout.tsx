6245a42b34904d80196454d7ce78df29d981a831
Alirezaw <Alirezawmoradi@gmail.com>
2024-08-18 16:35:22 +0330

    refactored

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,7 +2,7 @@ import {ReactNode} from "react";
 
 export default function DashboardLayout({children}: { children: ReactNode }) {
     return (
-        <main className='bg-gray-600 min-h-screen grid grid-rows-[80px_1fr_auto] overflow-x-hidden'>
+        <main className='bg-gray-600 min-h-screen grid grid-rows-[80px_1f] overflow-x-hidden'>
             <img src='/images/hero-bg.webp' alt='galaxy' width={1300} height={807}
                    className="fixed bottom-0 left-0 w-screen min-h-screen object-cover d-block pointer-events-none"/>
             {children}
