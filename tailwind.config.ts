130c78e04b287b694e8d5353a6da98bc745210f7
Alirezaw <Alirezawmoradi@gmail.com>
2024-08-19 20:01:11 +0330

    background image added to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,8 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'star-pattern': "url('/images/bg-stars-1.webp')",
+        'hero-pattern': "url('/images/footer-galaxy.webp')"
       },
     },
   },
