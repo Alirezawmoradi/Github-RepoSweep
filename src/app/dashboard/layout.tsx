57299f20ff94503e653c33b922997201d2dc51d3
Alirezaw <Alirezawmoradi@gmail.com>
2024-08-19 20:00:49 +0330

    background image set in layout file

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,14 +2,7 @@ import {ReactNode} from "react";
 
 export default function DashboardLayout({children}: { children: ReactNode }) {
     return (
-        <main className='relative bg-gray-600 min-h-screen grid grid-rows-[80px_1fr] overflow-x-hidden'>
-            <img
-                src='/images/hero-bg.webp'
-                alt='galaxy'
-                width={1300}
-                height={807}
-                className="fixed bottom-0 left-0 w-screen min-h-screen object-cover pointer-events-none"
-            />
+        <main className='relative  min-h-screen bg-star-pattern bg-cover bg-center bg-no-repeat grid grid-rows-[80px_1fr] overflow-x-hidden bg-black'>
             <div className="relative z-10">
                 {children}
             </div>
