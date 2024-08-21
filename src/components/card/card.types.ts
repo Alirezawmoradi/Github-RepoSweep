4b0c533d7e09996be213429d1bd655c87ca0523e
Alirezaw <Alirezawmoradi@gmail.com>
2024-08-21 23:22:28 +0330

    card types refactored

--- src/components/card/card.types.ts
+++ src/components/card/card.types.ts
@@ -1,11 +1,6 @@
-interface CardProps {
-    name: string;
-    description: string | null;
+interface CardProps extends Repository{
     isPrivate: boolean;
     url: string;
     onSelect: () => void;
     isSelected: boolean;
-    language: string;
-    stargazers_count: number;
-    topics: number[];
 }
