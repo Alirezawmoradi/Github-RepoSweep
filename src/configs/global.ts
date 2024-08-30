0a72a691b0cc92cc91b795429c079ad2d3462bf1
Alirezaw <Alirezawmoradi@gmail.com>
2024-08-30 15:02:49 +0330

    names refactored

--- src/configs/global.ts
+++ src/configs/global.ts
@@ -1,2 +1,2 @@
-export const clientId = process.env.GITHUB_ID;
-export const clientSecret = process.env.GITHUB_SECRET;
+export const clientId = process.env.AUTH_GITHUB_ID;
+export const clientSecret = process.env.AUTH_GITHUB_SECRET;
