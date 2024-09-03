592fb954cf22b995d4437886c719a8a1cbfe072f
Alirezaw <Alirezawmoradi@gmail.com>
2024-09-03 19:06:49 +0330

    refactored

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import {ReactNode} from "react";
 export default function DashboardLayout({children}: { children: ReactNode }) {
     return (
         <main className='relative  min-h-screen bg-star-pattern bg-cover bg-center bg-no-repeat grid grid-rows-[80px_1fr] overflow-x-hidden bg-black'>
-            <div className="relative z-10">
+            <div className="relative z-10 mt-20">
                 {children}
             </div>
         </main>
