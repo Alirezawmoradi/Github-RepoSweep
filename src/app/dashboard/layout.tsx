539b3fc49c5cc1cfde9a5b8f387fb39efb4ec791
Alirezaw <Alirezawmoradi@gmail.com>
2024-09-05 19:53:29 +0330

    background added to dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,11 +1,14 @@
 import {ReactNode} from "react";
+import {Background} from "@/components/background/background";
+import {DashboardHeroSection} from "@/app/dashboard/_components/dashboard-hero-section";
 
 export default function DashboardLayout({children}: { children: ReactNode }) {
     return (
         <main className='relative  min-h-screen bg-star-pattern bg-cover bg-center bg-no-repeat grid grid-rows-[80px_1fr] overflow-x-hidden bg-black'>
-            <div className="relative z-10 mt-20">
+            <div className="relative z-10">
                 {children}
             </div>
+            <Background/>
         </main>
     )
 }
