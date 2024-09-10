065042f7a53e52f702ae941adedf560f1c3170a3
Alirezaw <Alirezawmoradi@gmail.com>
2024-09-10 22:07:20 +0330

    refactored

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,11 +1,14 @@
-import {ReactNode} from "react";
+import { ReactNode } from "react";
 
-export default function DashboardLayout({children}: { children: ReactNode }) {
+export default function DashboardLayout({ children }: { children: ReactNode }) {
     return (
-        <main className='relative  min-h-screen bg-star-pattern bg-cover bg-center bg-no-repeat grid grid-rows-[80px_1fr] overflow-x-hidden bg-black'>
-            <div className="relative z-10">
+        <div className="relative min-h-screen bg-black">
+            <div
+                className="absolute -mt-10 bg-black inset-0 bg-star-pattern bg-cover bg-center bg-no-repeat bg-fixed z-0"
+            ></div>
+            <main className="relative z-10 min-h-screen  ">
                 {children}
-            </div>
-        </main>
-    )
+            </main>
+        </div>
+    );
 }
