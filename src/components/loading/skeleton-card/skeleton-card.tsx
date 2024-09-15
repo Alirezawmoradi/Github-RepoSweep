12f102d8ee6f23ceeb287213ff35693fcf98047e
Alirezaw <Alirezawmoradi@gmail.com>
2024-09-15 23:04:55 +0330

    skeleton card animation added for loading

--- src/components/loading/skeleton-card/skeleton-card.tsx
+++ src/components/loading/skeleton-card/skeleton-card.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SkeletonCard = () => {
     return (
-        <div className="border-gray-700 shadow-md rounded-lg w-full p-10 mx-auto bg-[#161b22]">
+        <div className="bg-[#161b22]/90 shadow-md rounded-lg w-full p-10 mx-auto bg-[#161b22]">
             <div className="flex justify-between items-center gap-10">
                 <div className="h-40 bg-slate-700 w-1/3 rounded-lg skeleton-text"
                      style={{animationDelay: '0.4s'}}></div>
