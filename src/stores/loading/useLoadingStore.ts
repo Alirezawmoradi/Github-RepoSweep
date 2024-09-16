79648aa2d1f15b3581b78914928a4818721a133d
Alirezaw <Alirezawmoradi@gmail.com>
2024-09-16 21:57:47 +0330

    initialLoad state and updater function added

--- src/stores/loading/useLoadingStore.ts
+++ src/stores/loading/useLoadingStore.ts
@@ -3,8 +3,10 @@ import {LoadingState} from "@/stores/loading/loading-state.types";
 
 export const useLoadingStore = create<LoadingState>((set) => ({
     loading: false,
+    initialLoad: true,
 
     actions: {
         setLoading: (isLoading) => set({loading: isLoading}),
+        setInitialLoad: (isLoading) => set({initialLoad: isLoading}),
     }
 }))
