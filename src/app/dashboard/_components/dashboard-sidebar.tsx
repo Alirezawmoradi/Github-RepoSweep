b24d36f7e19deeb69059378f520ad911c7db7007
Alirezaw <Alirezawmoradi@gmail.com>
2024-09-16 21:58:51 +0330

    refactored

--- src/app/dashboard/_components/dashboard-sidebar.tsx
+++ src/app/dashboard/_components/dashboard-sidebar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {useUserProfile} from "@/utils/user-profile-hook/useUserProfile";
+import {useUserData} from "@/utils/user-profile-hook/useUserData";
 import {useSession} from "next-auth/react";
 import {useModalStore} from "@/stores/modal/useModalStore";
 import {useRepoStore} from "@/stores/repository/useRepoStore";
@@ -11,22 +11,22 @@ import animatedLogo from "../../../../public/Animated-Github.json";
 
 export const DashboardSidebar: React.FC = () => {
     const {data: session} = useSession();
-    const userData = useUserProfile(session);
+    const {userData} = useUserData(session);
     const {openModal} = useModalStore((state) => state.actions);
     const selectedRepoCount = useRepoStore((state) => state.selectedRepos.size);
-    const {loading} = useLoadingStore();
+    const {loading, initialLoad} = useLoadingStore();
 
 
     return <aside className="col-span-1 xl:col-span-3 pl-10 sticky top-20 h-screen">
         <div className="flex flex-col items-center text-white">
-            {loading ?
+            {loading && initialLoad ?
                 <Lottie animationData={animatedLogo} className='w-64 h-64 animate-pulse'/>
                 :
-                <Avatar className={`w-64 h-64 ${loading ? 'border-none' : 'border-4'}`}
+                <Avatar className={`w-64 h-64`}
                         src={userData?.avatar_url} alt='Profile Image'/>
             }
             <div className="mt-4 text-center">
-                {loading ? (
+                {loading && initialLoad ? (
                     <div className="space-y-2 justify-center items-center flex flex-col">
                         <div className="w-32 h-3.5 bg-gray-700 rounded animate-pulse"/>
                         <div className="w-20 h-2 bg-gray-600 rounded animate-pulse"/>
@@ -39,14 +39,14 @@ export const DashboardSidebar: React.FC = () => {
                 )}
             </div>
             <div className="mt-4 text-center text-gray-300">
-                {loading ? (
+                {loading && initialLoad ? (
                     <div className="w-44 h-2.5 bg-gray-600 rounded animate-pulse"/>
                 ) : (
                     <p>{userData?.bio}</p>
                 )}
             </div>
             <div className="mt-6 text-center text-sm">
-                {loading ? (
+                {loading && initialLoad ? (
                     <>
                         <div className="w-20 h-2 bg-gray-600 rounded animate-pulse mx-auto"/>
                         <div className="w-36 h-2 bg-gray-600 rounded animate-pulse mt-2 mx-auto"/>
@@ -61,7 +61,7 @@ export const DashboardSidebar: React.FC = () => {
                         </p>
                     </>
                 )}
-                {loading ? (
+                {loading && initialLoad ? (
                     <div className='flex justify-center mt-2 gap-2'>
                         <div className="w-10 h-1.5 bg-gray-600 rounded animate-pulse "/>
                         <div className="w-10 h-1.5 bg-gray-600 rounded animate-pulse "/>
@@ -74,7 +74,7 @@ export const DashboardSidebar: React.FC = () => {
                 <div className="flex justify-between mt-10 relative">
                     <button
                         onClick={openModal}
-                        className={`bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded ${loading ? 'hidden' : 'block'}`}
+                        className={`bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded ${loading && initialLoad ? 'hidden' : 'block'}`}
                     >
                         Bulk Remove Selected
                         {
