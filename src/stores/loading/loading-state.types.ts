784763538da6136b814710e4f84945a7c61ddb69
Alirezaw <Alirezawmoradi@gmail.com>
2024-09-16 21:58:18 +0330

    initialLoad type and setInitialLoad action type added

--- src/stores/loading/loading-state.types.ts
+++ src/stores/loading/loading-state.types.ts
@@ -1,7 +1,9 @@
 export type LoadingState = {
     loading: boolean,
+    initialLoad:boolean,
 
     actions: {
         setLoading: (isLoading: boolean) => void,
+        setInitialLoad: (isLoading: boolean) => void,
     }
 }
