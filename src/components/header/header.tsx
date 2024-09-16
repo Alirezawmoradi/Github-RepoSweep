5847539e2e262835c214373fd5ade4dc1186bc6e
Alirezaw <Alirezawmoradi@gmail.com>
2024-09-16 19:38:49 +0330

    loading state added to profile avatar

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,10 +5,16 @@ import {AnimatedPlanet} from "@/components/animated-planet/animated-planet";
 import {useSession} from "next-auth/react";
 import {useUserProfile} from "@/utils/user-profile-hook/useUserProfile";
 import {HeaderItems} from "@/components/header/header-items";
+import {Avatar} from "@/components/avatar/avatar";
+import Lottie from "lottie-react";
+import animatedLogo from "../../../public/Animated-Github.json";
+import {useLoadingStore} from "@/stores/loading/useLoadingStore";
 
 export const Header: React.FC = () => {
     const {data: session} = useSession();
     const userData = useUserProfile(session);
+    const {loading} = useLoadingStore();
+
     return (
         <div className='flex container flex-col mx-auto mt-10'>
             <div className='flex flex-row justify-center items-center'>
@@ -17,11 +23,12 @@ export const Header: React.FC = () => {
                 </h1>
                 <div className='flex justify-center items-center mr-5 gap-2'>
                     <HeaderItems/>
-                    <img
-                        src={userData?.avatar_url}
-                        alt="Profile Image"
-                        className="w-12 h-12 rounded-full border-4 border-gray-800 shadow-md"
-                    />
+                    {loading ?
+                        <Lottie animationData={animatedLogo} className='w-12 h-12 animate-pulse'/>
+                        :
+                        <Avatar className={`w-12 h-12 ${loading ? 'border-none' : 'border-4'}`}
+                                src={userData?.avatar_url} alt='Profile Image'/>
+                    }
                 </div>
 
             </div>
