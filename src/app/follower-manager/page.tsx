b1109d224ca1119963752ad2419d504d9223614a
Alirezaw <Alirezawmoradi@gmail.com>
2024-11-22 14:59:24 +0330

    UserFollowerManager component created

--- src/app/follower-manager/page.tsx
+++ src/app/follower-manager/page.tsx
@@ -1,5 +1,5 @@
+import {UserFollowerManager} from "@/app/follower-manager/_components/follower-manager";
+
 export default async function FollowerManager() {
-    return (
-        <h2 className='text-white text-xl'>Follower manager</h2>
-    )
+    return <UserFollowerManager/>
 }
