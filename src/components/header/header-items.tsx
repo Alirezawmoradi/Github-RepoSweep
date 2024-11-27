e62af97ca359ee0eee2722de1c9cc1275df4312b
Alirezaw <Alirezawmoradi@gmail.com>
2024-11-27 23:25:43 +0330

    styles changed

--- src/components/header/header-items.tsx
+++ src/components/header/header-items.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import {GithubIcon} from "@/components/icons/GithubIcon";
 import {signOut} from "next-auth/react";
 import {LogoutButton} from "@/components/auth-button/logout-button";
+import {SlUserFollowing} from "react-icons/sl";
 
 export const HeaderItems: React.FC<HeaderProps> = ({userData}) => {
     return (
@@ -14,10 +15,14 @@ export const HeaderItems: React.FC<HeaderProps> = ({userData}) => {
                     Source Code
                 </div>
             </Link>
-            <LogoutButton text={'Logout'} onClick={() => signOut()}/>
             <Link href={`/follower-manager?username=${userData?.login}`}>
-                <h1>Follower Manager</h1>
+                <div
+                    className="flex gap-2 px-2 py-1 rounded-xl justify-center items-center hover:bg-gray-700 transition-colors duration-300">
+                    <SlUserFollowing width={24} height={24}/>
+                    Follower Manager
+                </div>
             </Link>
+            <LogoutButton text={'Logout'} onClick={() => signOut()}/>
         </div>
     )
 }
